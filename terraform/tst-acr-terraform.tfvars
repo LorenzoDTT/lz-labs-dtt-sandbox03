e837267438ab6cd1a35d42f94bee1e314c53d315
Lorenzo Ortega <lorenzo.ortega@freebyte.es>
2026-02-15 19:51:38 +0100

    solution

--- terraform/tst-acr-terraform.tfvars
+++
@@ -1,3 +0,0 @@
-solution_name = "ACX"
-aws_region = "eu-south-2"
-environment_prefix = "acx-test"
---
+++ terraform/variables.tf
@@ -0,0 +1,3 @@
+variable "environment_prefix" { type = string }
+variable "solution_name"      { type = string }
+variable "aws_region"         { type = string }
